77df873f0c1c2338f2b8b8e06f49ebb5ab8ea7f9
Jossip <Jossip@DESKTOP-AIT09KN>
2016-06-03 00:12:08 -0500

    Ejecutado correctamente

--- Gestor/src/gestor/Gestor.java
+++ Gestor/src/gestor/Gestor.java
@@ -2,5 +2,29 @@
 package gestor;
 
 public class Gestor {
+    public Gestor(){}
+    
+    private String comando;
+    
+    public void lanzarApp(String comando)
+    {
+        this.comando = comando;
+        Runtime aplicacion = Runtime.getRuntime(); 
+        switch (comando)
+        {
+            case "BLOCK DE NOTAS":
+                    try{aplicacion.exec("C:/Windows/System32/NOTEPAD.EXE"); }
+                    catch(Exception e){}
+                    break;
+            case "WORD":
+                    try{aplicacion.exec("C:\\Program Files\\Microsoft Office\\Office15\\WINWORD.EXE"); }
+                    catch(Exception e){}
+                    break;
+        }
+        
 
+    
+    
+    }
+    
 }
